73f778a2daaf609930230c33471aa4f02dc974b4
biswajitecd <biswajit@ethicalden.com>
2025-12-22 06:09:13 +0530

    cash on delivery compleate but razor pay not working

--- src/Pages/CheckOut/CheckOut.jsx
+++ src/Pages/CheckOut/CheckOut.jsx
@@ -1,56 +1,183 @@
 import React, { useContext, useEffect, useState } from "react";
 import Button from "@mui/material/Button";
-import { BsBagCheckFill } from "react-icons/bs";
+import { BsBagCheckFill, BsFillBagCheckFill } from "react-icons/bs";
 import { myContext } from "../../App";
 import { FaPlus } from "react-icons/fa6";
 import Radio from "@mui/material/Radio";
+import { deleteData, fetchDataFromApi, postData } from "../../utlis/api";
+import { useNavigate } from "react-router-dom";
+
+// payemt getway Razor Pay
+const VITE_APP_RAZORPAY_KEY_ID = import.meta.env.VITE_APP_RAZORPAY_KEY_ID;
+const VITE_APP_RAZORPAY_KEY_SECRET = import.meta.env
+  .VITE_APP_RAZORPAY_KEY_SECRET;
 
 const CheckOut = () => {
+  const [userData, setUserData] = useState(null);
   const [isChecked, setIsChecked] = useState(0);
+  const [selectedAddress, setSelectedAddress] = useState("");
+  const [totalAmount, setTotalAmount] = useState();
 
   const context = useContext(myContext);
+  const navigate = useNavigate();
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
+  useEffect(() => {
+    setUserData(context?.userData);
+    setSelectedAddress(context?.userData?.address_details[0]?._id);
+    fetchDataFromApi(`/api/order/order-list`).then((res) => {
+      console.log("order-list", res);
+    });
+  }, [context?.userData, userData]);
+
+  useEffect(() => {
+    setTotalAmount(
+      context?.cartData?.length !== 0
+        ? context?.cartData
+            ?.map((item) => parseInt(item.price) * item?.quantity)
+            .reduce((total, value) => total + value, 0)
+        : 0
+    )?.toLocaleString("en-US", { style: "currency", currency: "INR" });
+
+    // localStorage.setItem("totalAmount", context?.cartData?.length !==0 ?
+    //  context?.cartData?.map(item=>parseInt(item.price)* item?.quantity).reduce((total, value)=> total+value,0):0)
+    //  ?.toLocaleString('en-US',{style:'currency', currency:'INR'});
+  }, [context?.cartData]);
 
   const editAddress = (id) => {
     context?.setOpenAddressPanel(true);
     context?.setAddressMode("edit");
     context?.setAddressId(id);
   };
+
   const handelChange = (e, index) => {
     if (e.target.checked) {
       setIsChecked(index);
+      setSelectedAddress(e.target.value);
     }
+  };
+
+  const checkout = (e) => {
+    e.preventDefault();
+
+    var options = {
+      key: VITE_APP_RAZORPAY_KEY_ID,
+      key_secret: VITE_APP_RAZORPAY_KEY_SECRET,
+      amount: parseInt(totalAmount * 100),
+      currency: "INR",
+      order_receipt: "order_rcptid_" + context?.userData?.name,
+      name: "B-Mart megashop",
+      description: "for testing purpose",
+      handler: function (response) {
+        console.log(response);
+
+        const paymentId = response?.razorpay_payment_id;
+        const user = context?.userData;
+
+        const payLoad = {
+          userId: user?._id,
+          products: context?.cartData,
+          paymentId: paymentId,
+          payment_status: "COMPLEATED",
+          delivery_address: selectedAddress,
+          totalAmt: totalAmount,
+          date: new Date().toLocaleString("en-US", {
+            month: "short",
+            day: "2-digit",
+            year: "numeric",
+          }),
+        };
+
+        console.log("payLoad", payLoad);
+
+        postData(`/api/order/create`, payLoad).then((res) => {
+          if (res?.data?.error === false) {
+            context.openAlartBox("Sucess", res?.message);
+            deleteData(`/api/cart/emptyCart/${user?._id}`).then((res) => {
+              context?.getCartItems();
+            });
+            navigate("/");
+          } else {
+            context.openAlartBox("Error", res?.message);
+          }
+        });
+      },
+      theme: {
+        color: "#3399cc",
+      },
+    };
+
+    var pay = new window.Razorpay(options);
+    pay.open();
+  };
+
+  const cashOnDelivery = (e) => {
+    e.preventDefault();
+
+    const user = context?.userData;
+
+    // const totalAmount =
+    //   context?.cartData?.length !== 0
+    //     ? context.cartData.reduce(
+    //         (sum, item) => sum + item.price * item.quantity,
+    //         0
+    //       )
+    //     : 0;
+
+    const payLoad = {
+      userId: user?._id,
+      products: context?.cartData,
+      paymentId: "",
+      payment_status: "Cash on Delivery",
+      delivery_address: selectedAddress,
+      totalAmt: totalAmount,
+      date: new Date().toLocaleString("en-US", {
+        month: "short",
+        day: "2-digit",
+        year: "numeric",
+      }),
+    };
+
+    postData(`/api/order/create`, payLoad).then((res) => {
+      context.openAlartBox("Sucess", res?.message);
+      if (res?.error === false) {
+        deleteData(`/api/cart/emptyCart/${user?._id}`).then((res) => {
+          context?.getCartItems();
+        });
+        navigate("/");
+      } else {
+        context.openAlartBox("Error", res?.message);
+      }
+    });
   };
 
   return (
     <section className="py-10">
-      <div className="w-[70%] m-auto flex gap-[30px]">
-        <div className="leftCol w-[60%]">
-          <div className="cart bg-white shadow-md p-5 rounded-md w-full">
-            <div className="flex items-center justify-between">
-              <h2>Select Delivery Address</h2>
-              <Button
-                className="flex items-center gap-2"
-                variant="outlined"
-                onClick={() => {
-                  context?.setOpenAddressPanel(true);
-                  context?.setAddressMode("add");
-                }}
-              >
-                <FaPlus />
-                Add New Address
-              </Button>
-            </div>
-            <br />
+      <form onSubmit={checkout}>
+        <div className="w-[70%] m-auto flex gap-[30px]">
+          <div className="leftCol w-[60%]">
+            <div className="cart bg-white shadow-md p-5 rounded-md w-full">
+              <div className="flex items-center justify-between">
+                <h2>Select Delivery Address</h2>
+                <Button
+                  className="flex items-center gap-2"
+                  variant="outlined"
+                  onClick={() => {
+                    context?.setOpenAddressPanel(true);
+                    context?.setAddressMode("add");
+                  }}
+                >
+                  <FaPlus />
+                  Add New Address
+                </Button>
+              </div>
+              <br />
 
-            <div className="flex flex-col gap-4">
-              {context?.userData?.address_details?.length !== 0 ? (
-                context?.userData?.address_details
-                  ?.slice()
-                  ?.reverse()
-                  ?.map((address, index) => {
+              <div className="flex flex-col gap-4">
+                {userData?.address_details?.length !== 0 ? (
+                  userData?.address_details?.map((address, index) => {
                     return (
                       <label
                         key={index}
@@ -63,6 +190,7 @@ const CheckOut = () => {
                             size="small"
                             onChange={(e) => handelChange(e, index)}
                             checked={isChecked == index}
+                            value={address?._id}
                           />
                         </div>
                         <div className="info">
@@ -95,91 +223,106 @@ const CheckOut = () => {
                       </label>
                     );
                   })
-              ) : (
-                <>
-                  <div className="flex items-center mt-5 justify-between flex-col p-5">
-                    <img
-                      src="/maps-and-location.png"
-                      alt=""
-                      className="w-[150px]"
-                    />
-                    <h2 className="text-center">
-                      Address not found in your account
-                    </h2>
-                    <p className="!mt-0">Add a delivery address</p>
-                    <Button
-                      className="btn-org btn-sm flex items-center gap-2"
-                      onClick={() => {
-                        context?.setOpenAddressPanel(true);
-                        context?.setAddressMode("add");
-                      }}
-                    >
-                      <FaPlus size={16} />
-                      Add New Address
-                    </Button>
-                  </div>
-                </>
-              )}
-            </div>
-          </div>
-        </div>
-
-        <div className="rightcol w-[40%]">
-          <div className="card shadow-md bg-white p-5 rounded-md">
-            <h2 className="mb-4">Your Order</h2>
-            <div className="flex items-center justify-between py-3 border-t border-b border-[rgba(0,0,0,0.1)]">
-              <span className="text-[14px] font-[600]">Product</span>
-              <span className="text-[14px] font-[600] pr-5">Subtotal</span>
-            </div>
-
-            <div className="mb-5 scroll max-h-[300px] overflow-y-scroll overflow-x-hidden pr-2">
-              {context?.cartData?.length !== 0 &&
-                context?.cartData?.map((item, index) => {
-                  return (
-                    <div
-                      className="flex items-center justify-between py-2"
-                      key={index}
-                    >
-                      <div className="part1 flex items-center gap-3">
-                        <div className="img w-[50px] h-[50px]  object-cover overflow-hidden rounded-md group cursor-pointer">
-                          <img
-                            src={item?.image}
-                            alt=""
-                            className="w-full transition-all group-hover:scale-105"
-                          />
-                        </div>
-                        <div className="info">
-                          <h4
-                            className="text-[14px]"
-                            title={item?.productTitle}
-                          >
-                            {item?.productTitle?.length > 28
-                              ? item?.productTitle?.substring(0, 28) + "..."
-                              : item?.productTitle}
-                          </h4>
-                          <span className="text-[13px]">
-                            Qty : {item?.quantity}
-                          </span>
-                        </div>
-                      </div>
-                      <span className="text-[14px] font-[600]">
-                        {(item?.quantity * item?.price)?.toLocaleString(
-                          "en-US",
-                          { style: "currency", currency: "INR" }
-                        )}
-                      </span>
+                ) : (
+                  <>
+                    <div className="flex items-center mt-5 justify-between flex-col p-5">
+                      <img
+                        src="/maps-and-location.png"
+                        alt=""
+                        className="w-[150px]"
+                      />
+                      <h2 className="text-center">
+                        Address not found in your account
+                      </h2>
+                      <p className="!mt-0">Add a delivery address</p>
+                      <Button
+                        className="btn-org btn-sm flex items-center gap-2"
+                        onClick={() => {
+                          context?.setOpenAddressPanel(true);
+                          context?.setAddressMode("add");
+                        }}
+                      >
+                        <FaPlus size={16} />
+                        Add New Address
+                      </Button>
                     </div>
-                  );
-                })}
+                  </>
+                )}
+              </div>
             </div>
+          </div>
 
-            <Button className="btn-org btn-lg btn-sm w-full flex items-center gap-2">
-              <BsBagCheckFill size={22} />
-              Checkout
-            </Button>
+          <div className="rightcol w-[40%]">
+            <div className="card shadow-md bg-white p-5 rounded-md">
+              <h2 className="mb-4">Your Order</h2>
+              <div className="flex items-center justify-between py-3 border-t border-b border-[rgba(0,0,0,0.1)]">
+                <span className="text-[14px] font-[600]">Product</span>
+                <span className="text-[14px] font-[600] pr-5">Subtotal</span>
+              </div>
+
+              <div className="mb-5 scroll max-h-[300px] overflow-y-scroll overflow-x-hidden pr-2">
+                {context?.cartData?.length !== 0 &&
+                  context?.cartData?.map((item, index) => {
+                    return (
+                      <div
+                        className="flex items-center justify-between py-2"
+                        key={index}
+                      >
+                        <div className="part1 flex items-center gap-3">
+                          <div className="img w-[50px] h-[50px]  object-cover overflow-hidden rounded-md group cursor-pointer">
+                            <img
+                              src={item?.image}
+                              alt=""
+                              className="w-full transition-all group-hover:scale-105"
+                            />
+                          </div>
+                          <div className="info">
+                            <h4
+                              className="text-[14px]"
+                              title={item?.productTitle}
+                            >
+                              {item?.productTitle?.length > 28
+                                ? item?.productTitle?.substring(0, 28) + "..."
+                                : item?.productTitle}
+                            </h4>
+                            <span className="text-[13px]">
+                              Qty : {item?.quantity}
+                            </span>
+                          </div>
+                        </div>
+                        <span className="text-[14px] font-[600]">
+                          {(item?.quantity * item?.price)?.toLocaleString(
+                            "en-US",
+                            { style: "currency", currency: "INR" }
+                          )}
+                        </span>
+                      </div>
+                    );
+                  })}
+              </div>
+
+              <div className="flex items-center flex-col gap-2 mb-2">
+                <Button
+                  type="submit"
+                  className="btn-org btn-lg w-full flex items-center gap-2"
+                >
+                  <BsBagCheckFill size={22} />
+                 Pay Online
+                </Button>
+
+                <Button
+                  type="button"
+                  className="btn-dark btn-lg w-full flex gap-2 items-end"
+                  onClick={cashOnDelivery}
+                >
+                  <BsFillBagCheckFill size={22} />
+                  Cash on Delivery
+                </Button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </form>
     </section>
   );
 };
